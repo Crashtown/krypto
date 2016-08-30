93aeeb9b4f1b32a331fcc59066fbdcfd27dc1cba
Anton Zhiliuk <crashtown.pal@gmail.com>
2016-08-30 08:31:32 +0300

    create interface functions

--- lib/krypto.ex
+++ lib/krypto.ex
@@ -1,2 +1,11 @@
 defmodule Krypto do
+  def encrypt_public(data) when is_binary(data) do
+    pubkey = Krypto.Server.get_pubkey
+    :public_key.encrypt_public(data, pubkey)
+  end
+
+  def decrypt_private(cipher) when is_binary(cipher) do
+    privkey = Krypto.Server.get_privkey
+    :public_key.decrypt_private(cipher, privkey)
+  end
 end
